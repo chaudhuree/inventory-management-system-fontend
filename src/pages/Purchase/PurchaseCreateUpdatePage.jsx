5544d7f97d0fb9fd94c71724f9f22af263b1f751
chaudhuree <chaudhuree00@gmail.com>
2023-05-03 03:02:45 +0600

    19 - purchase create show related task done

--- src/pages/Purchase/PurchaseCreateUpdatePage.jsx
+++ src/pages/Purchase/PurchaseCreateUpdatePage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {Fragment, Suspense} from 'react';
+import MasterLayout from "../../components/MasterLayout/MasterLayout";
+import LazyLoader from "../../components/MasterLayout/LazyLoader";
+import PurchaseCreateUpdate from "../../components/Purchase/PurchaseCreateUpdate";
 
-export default function PurchaseCreateUpdatePage() {
-  return (
-    <div>PurchaseCreateUpdatePage</div>
-  )
-}
+const PurchaseCreateUpdatePage = () => {
+    return (
+        <Fragment>
+            <MasterLayout>
+                <Suspense fallback={<LazyLoader/>}>
+                    <PurchaseCreateUpdate/>
+                </Suspense>
+            </MasterLayout>
+        </Fragment>
+    );
+};
+
+export default PurchaseCreateUpdatePage;
