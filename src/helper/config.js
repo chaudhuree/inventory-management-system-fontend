0ed6ca62b43f960191e4c4e1212d01c52f79a525
chaudhuree <chaudhuree00@gmail.com>
2023-09-09 02:42:05 +0600

    replaced base url with deployed server

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,1 +1,2 @@
-export const BaseURL = "http://localhost:3000/api/v1";
+// export const BaseURL = "http://localhost:3000/api/v1";
+export const BaseURL = "https://inventory-management-system-chaudhuree.onrender.com/api/v1";
